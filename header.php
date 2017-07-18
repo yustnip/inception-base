89b30b3aa7a72aaa46f90b8f6e41235b4d1cc945
Nikita Yustovskiy <yustovskiy@ya.ru>
2017-07-19 00:27:19 +0300

    Fixed .gitignore for templates/blocks

--- header.php
+++ header.php
@@ -3,8 +3,8 @@
 
 <head>
   <meta charset="utf-8">
-  <!-- meta( name="viewport", content="width=device-width, initial-scale=1" ) -->
-  <!-- meta( name="viewport", content="width=950" ) -->
+  <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+  <!-- <meta name="viewport" content="width=950" -->
   <?php wp_head(); ?>
 </head>
 
