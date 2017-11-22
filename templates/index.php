e46a1f7fe70a20826c83436408f8a10a50a6523b
Nikita Yustovskiy <yustovskiy@ya.ru>
2017-11-22 16:41:07 +0300

    Added Jeet

--- templates/index.php
+++ templates/index.php
@@ -1,22 +1,17 @@
 <?php get_header(); ?>
 
 <div class="container">
-  <div class="row">
-    <div class="col-md-12">
-
-      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-      <?php the_content(); ?>
-      <?php endwhile; else : ?>
-      <div class="loop-error">
-        <p class="loop-error__message example">
-          <?php _e('Sorry, no posts matched your criteria.'); ?>
-        </p>
-      </div>
-      <?php endif; ?>
-
-      <p class="theme-name">Inception Base</p>
-    </div>
+  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+  <?php the_content(); ?>
+  <?php endwhile; else : ?>
+  <div class="loop-error">
+    <p class="loop-error__message example">
+      <?php _e('Sorry, no posts matched your criteria.'); ?>
+    </p>
   </div>
+  <?php endif; ?>
+
+  <p class="theme-name">Inception Base</p>
 </div>
 
 <?php get_footer(); ?>
