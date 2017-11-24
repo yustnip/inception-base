9a34c69c0aa7bf538f4aa224b4f39ea4252f289d
Nikita Yustovskiy <yustovskiy@ya.ru>
2017-11-24 14:35:53 +0300

    Improved gulp-posthtml config

---
+++ templates/index.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<div class="container">
+  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+  <?php the_content(); ?>
+  <?php endwhile; else : ?>
+  <div block="loop-error">
+    <p elem="message" class="example">
+      <?php _e('Sorry, no posts matched your criteria.'); ?>
+    </p>
+  </div>
+  <?php endif; ?>
+
+  <p class="theme-name">Inception Base</p>
+</div>
+
+<?php get_footer(); ?>
